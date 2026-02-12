70601bacb194a405a7b5529b39ed1edf99d16915
Jayanth <Jayanth.reddy@cardyai.com>
2026-02-12 16:27:12 +0530

    changed to pydantic v2

--- UserRouter.py
+++ UserRouter.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter ,Depends
 from PydanticModel import UserRequest
 from PydanticModel import CreateUserResponse ,UserResponse
 from MongoDataBase import get_database
+from bson import ObjectId
+
 router = APIRouter(prefix = "/user" ,tags = ["userRouter"])
 
 @router.post("/",response_model = CreateUserResponse)
 async def createUsers(user : UserRequest ,db = Depends(get_database)):
-    user_dict = user.dict(by_alias = True)
+    user_dict = user.model_dump(by_alias = True)
     user_dict["fromApi"] = True
     result =await db.users.insert_one(user_dict)
     return {"id" : str(result.inserted_id)}
@@ -15,6 +17,7 @@ async def createUsers(user : UserRequest ,db = Depends(get_database)):
 async def getUser( userId : str , db = Depends(get_database) ):
 
     user = await db.users.find_one({ "_id" : ObjectId(userId) })
+    user["_id"] = str(user["_id"])
     return user
 
 
